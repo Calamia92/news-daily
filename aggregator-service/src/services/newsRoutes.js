5d64e1cf5a2aaf75f7d0a840478bce31928e595e
Hicham <hicham.guendouz77500@gmail.com>
2025-03-21 21:34:59 +0100

    chore: suppression des fichiers vides et redondants suite à la restructuration microservice

--- aggregator-service/src/services/newsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const newsController = require('./newsController'); // Updated path
-
-// Define the /news endpoint
-router.get('/news', newsController.getNews);
-
-module.exports = router;
